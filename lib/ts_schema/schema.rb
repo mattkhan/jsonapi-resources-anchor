940851bc22e6725051085ddcd22df6630be0661a
mattkhan <mattkhan@users.noreply.github.com>
2024-07-21 19:48:11 -0700

    Fix nil error

--- lib/ts_schema/schema.rb
+++ lib/ts_schema/schema.rb
@@ -18,8 +18,8 @@ module TSSchema
       # @return [String]
       def generate(context: {}, include_all_fields: true)
         maybe_type = "type Maybe<T> = T | null;"
-        enums = @enums.map(&:to_ts_type_string)
-        types = @resources.map { |t| t.to_ts_type_string(context:, include_all_fields:) }
+        enums = (@enums || []).map(&:to_ts_type_string)
+        types = (@resources || []).map { |r| r.to_ts_type_string(context:, include_all_fields:) }
 
         ([maybe_type] + enums + types).join("\n\n") + "\n"
       end
