9ecd8e3bc347c6765ace68fe58770600fe19c03e
mattkhan <mattkhan@users.noreply.github.com>
2024-08-15 16:08:24 -0700

    Don't delete description from options

--- lib/anchor/concerns/annotatable.rb
+++ lib/anchor/concerns/annotatable.rb
@@ -15,7 +15,7 @@ module Anchor
           opts = annotation_or_options.is_a?(Hash) ? annotation_or_options : options
           annotation_given = !(annotation_or_options.is_a?(Hash) || annotation_or_options.nil?)
           @anchor_attributes[name] = annotation_or_options if annotation_given
-          @anchor_attributes_descriptions[name] = opts.delete(:description)
+          @anchor_attributes_descriptions[name] = opts[:description]
           super(name, opts)
         end
 
@@ -28,7 +28,7 @@ module Anchor
           opts = annotation_or_options.is_a?(Hash) ? annotation_or_options : options
           annotation_given = !(annotation_or_options.is_a?(Hash) || annotation_or_options.nil?)
           @anchor_relationships[name] = annotation_or_options if annotation_given
-          @anchor_relationships_descriptions[name] = opts.delete(:description)
+          @anchor_relationships_descriptions[name] = opts[:description]
           super(name, opts)
         end
       end
