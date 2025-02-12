ecef0ee78152f26878bd5197c07a0a150bc02246
mattkhan <86168986+mattkhan@users.noreply.github.com>
2025-02-12 13:29:40 -0800

    chore: fix rake eager loading for polymorphic type inference

--- spec/example/config/application.rb
+++ spec/example/config/application.rb
@@ -22,6 +22,7 @@ module Example
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.rake_eager_load = true
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
