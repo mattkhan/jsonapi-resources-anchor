075aafba7774c8fd9f5ce6846d7bd0424b4650fc
mattkhan <86168986+mattkhan@users.noreply.github.com>
2025-07-21 10:47:06 -0700

    feat: raise when registering duplicate type name

--- lib/anchor/schema.rb
+++ lib/anchor/schema.rb
@@ -1,5 +1,7 @@
 module Anchor
   class Schema
+    class DuplicateTypeError < StandardError; end
+
     class << self
       Register = Struct.new(:resources, :enums, keyword_init: true)
 
@@ -9,6 +11,9 @@ module Anchor
 
       def resource(resource)
         @resources ||= []
+        if @resources.map(&:anchor_schema_name).include?(resource.anchor_schema_name)
+          raise DuplicateTypeError, "A resource with type name '#{resource.anchor_schema_name}' has already been registered."
+        end
         @resources.push(resource)
       end
 
